71522447869d19ae1df8f789ca5d09fece1128f4
JulianChu <yulang.chu@gmail.com>
2018-04-10 23:25:15 +0200

    add failed test: give 5 return V

--- test/IntegerToRoman.test.ts
+++ test/IntegerToRoman.test.ts
@@ -4,4 +4,8 @@ describe("convert to single Alphabet",()=>{
   it("give 1 return I", ()=>{
     expect(integerToRoman(1)).toBe("I");
   });
+
+  it("give 5 return V",()=>{
+    expect(integerToRoman(5)).toBe("V");
+  });
 })
