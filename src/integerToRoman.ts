6ab4f78cc6aae63b6d00cea892dc0b0150a2d892
JulianChu <yulang.chu@gmail.com>
2018-04-10 23:49:13 +0200

    add tests for basic numerals

--- src/integerToRoman.ts
+++ src/integerToRoman.ts
@@ -1,6 +1,11 @@
 function integerToRoman(integer:number):string{
-  if(integer == 1)  return "I";
-  if(integer == 5)  return "V";
+  if(integer === 1)  return "I";
+  if(integer === 5)  return "V";
+  if(integer === 10) return "X";
+  if(integer === 50) return "L";
+  if(integer === 100) return "C";
+  if(integer === 500) return "D";
+  if(integer === 1000) return "M";
   return "";
 }
 
--- test/IntegerToRoman.test.ts
+++ test/IntegerToRoman.test.ts
@@ -8,4 +8,25 @@ describe("convert to single Alphabet",()=>{
   it("give 5 return V",()=>{
     expect(integerToRoman(5)).toBe("V");
   });
+
+  it("give 10 return X",()=>{
+    expect(integerToRoman(10)).toBe("X");
+  });
+
+  it("give 50 return L",()=>{
+    expect(integerToRoman(50)).toBe("L");
+  })
+
+  it("give 100 return C", ()=>{
+    expect(integerToRoman(100)).toBe("C");
+  });
+
+  it("give 500 return D",()=>{
+    expect(integerToRoman(500)).toBe("D");
+  });
+
+  it("give 1000 return M",()=>{
+    expect(integerToRoman(1000)).toBe("M");
+  })
+
 })
