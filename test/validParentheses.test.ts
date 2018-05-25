597a20dd0a5199d8b1b78b371ac50b1a1fb6c15c
yu-lang.chu <yu-lang.chu@redheads.de>
2018-05-25 12:28:15 +0200

    2nd testcase (]

--- test/validParentheses.test.ts
+++ test/validParentheses.test.ts
@@ -3,6 +3,10 @@ import isValid from "../src/validParentheses";
 describe('Test isValid()',()=>{
     it('give()', ()=>{
         expect(isValid("()")).toBe(true);
+    });
+
+    it('give(]', ()=>{
+        expect(isValid("(]")).toBe(false);
     })
 
 
