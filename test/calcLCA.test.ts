95a21c8f2a4e926f49cf4af776578f451bbf83f1
JulianChu <yulang.chu@gmail.com>
2018-08-22 22:05:47 +0200

    add failed case: p=5,q=8

--- test/calcLCA.test.ts
+++ test/calcLCA.test.ts
@@ -7,7 +7,13 @@ import {
 describe("calc Lower Common Ancestor", () => {
   let root = createBinaryTree();
 
-  it("give defined root, p = 5, q = 1, return 5", () => {
-    expect(calcLCA(root, new TreeNode(5), new TreeNode(1))).toBe(3);
+  describe("give defined root", () => {
+    it("p = 5, q = 1, return 3", () => {
+      expect(calcLCA(root, new TreeNode(5), new TreeNode(1))).toBe(3);
+    });
+
+    it("p=5, q=8, return 3", () => {
+      expect(calcLCA(root, new TreeNode(5), new TreeNode(8))).toBe(3);
+    });
   });
 });
