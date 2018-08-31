a3bdda47320ad8fd995ebc7fc4f82fb35bfa4154
JulianChu <yulang.chu@gmail.com>
2018-09-01 00:14:49 +0200

    passed 2nd test case

--- src/LeetCode_671_SecondMiniNodeInBTree/findSecondMinimumValue.ts
+++ src/LeetCode_671_SecondMiniNodeInBTree/findSecondMinimumValue.ts
@@ -1,6 +1,24 @@
 export let findSecondMinimumValue = function(root: TreeNode): number {
-  return -1;
+  let nodeList = new Set<number>();
+
+  findLeafNode(root, nodeList);
+  if (nodeList.size === 1) return -1;
+
+  let max = Math.max(...nodeList);
+  nodeList.delete(max);
+  let secondMinVal = Math.max(...nodeList);
+
+  return secondMinVal;
 };
+
+function findLeafNode(root: TreeNode, nodeList: Set<number>) {
+  if (!root.left && !root.right) {
+    nodeList.add(root.val);
+  } else {
+    root.left && findLeafNode(root.left, nodeList);
+    root.right && findLeafNode(root.right, nodeList);
+  }
+}
 
 export class TreeNode {
   val: number;
