f926cb9ffbed39112c266a69c8aa3f849a7ee42d
JulianChu <yulang.chu@gmail.com>
2018-09-07 23:59:02 +0200

    add first test case: failed

---
+++ src/LeetCode_504_base7/convertToBase7.ts
@@ -0,0 +1,7 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+export function convertToBase7(num: number): string {
+  return "";
+}
---
+++ test/convertToBase7.test.ts
@@ -0,0 +1,7 @@
+import { convertToBase7 } from "../src/LeetCode_504_base7/convertToBase7";
+
+describe("convert to base 7", () => {
+  it("give 7, return '10'", () => {
+    expect(convertToBase7(7)).toBe("10");
+  });
+});
