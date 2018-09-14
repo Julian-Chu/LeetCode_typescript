e2923330888620143e48193cb91ec04da12c47ce
JulianChu <yulang.chu@gmail.com>
2018-09-14 22:42:56 +0200

    pass first test

--- src/LeetCode_504_base7/convertToBase7.ts
+++ src/LeetCode_504_base7/convertToBase7.ts
@@ -3,5 +3,14 @@
  * @return {string}
  */
 export function convertToBase7(num: number): string {
-  return "";
+  let resArr = [];
+  let quotient: number;
+  let remainder = num;
+  while (remainder >= 7) {
+    quotient = Math.floor(remainder / 7);
+    remainder = remainder % 7;
+    resArr.push(quotient);
+  }
+  resArr.push(remainder);
+  return resArr.join("");
 }
